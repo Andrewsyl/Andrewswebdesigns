e2efa6d17cd72983cb17485f6dffe8b52ea9b230
unknown <andrewsylcostello@gmail.com>
2016-01-05 17:01:21 +0000

    first comment

---
+++ contact/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from contact.models import ContactForm
+from django import forms
+
+class ContactView(ModelForm):
+    message = forms.CharField(widget=forms.Textarea)
+
+    class Meta:  # fields that will be on webpage
+        model = ContactForm
+        fields = ('name', 'email', 'topic', 'message')
