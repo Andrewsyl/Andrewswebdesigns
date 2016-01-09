e900faf573018772d66f5d2c684bbcfb84e4f5bd
Andrewsyl <andrew.costello@ucdconnect.ie>
2016-01-09 17:04:04 +0000

    Update urls.py

--- AndrewDesign/urls.py
+++ AndrewDesign/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + \
               static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if not settings.DEBUG:
+
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
