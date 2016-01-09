0eaae4a2a0d66aff48c221bc8a2ef15945b8051f
Andrewsyl <andrew.costello@ucdconnect.ie>
2016-01-09 16:52:11 +0000

    Update settings.py

--- AndrewDesign/settings.py
+++ AndrewDesign/settings.py
@@ -195,10 +195,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
-try:
-    from local_settings import *
-except Exception as e:
-    print e.message
     
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
